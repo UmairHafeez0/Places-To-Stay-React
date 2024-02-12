94868f64edd2081708e670095817853a04a6dcae
UmairHafeez0 <130215318+UmairHafeez0@users.noreply.github.com>
2024-02-12 19:30:00 +0500

    Add Singup and Input Layout Fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,54 +6,7 @@ import { auth, db } from '../firebaseConfig';
 import { doc, getDoc } from "firebase/firestore";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const router = useRouter();
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        const userDoc = await getDoc(doc(db, 'users', user.uid));
-        const userData = userDoc.data();
-        if (userData?.category === 'admin') {
-          router.push('/AdminDashboard');
-        } else {
-          router.push('/home');
-        }
-      }
-    });
-    return () => unsubscribe();
-  }, [router]);
-
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setError(''); 
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-    } catch (error) {
-      
-      if (error instanceof Error) {
-       
-        const errorCode = (error as any).code;
-  
-        if (errorCode === 'auth/wrong-password') {
-          setError('Invalid password. Please try again.');
-        } else if (errorCode === 'auth/user-not-found') {
-          setError('No user found with this email address.');
-        } else if (errorCode === 'auth/invalid-email') {
-          setError('Invalid email format.');
-        } else {
-          setError('No User Found.');
-        }
-        console.error('Error Logging in: ', error);
-      } else {
-        setError('An unexpected error occurred. Please try again later.');
-        console.error('Unexpected error:', error);
-      }
-    }
-  };
-  
 
   return (
     <div className="container mt-5">
@@ -74,25 +27,7 @@ export default function LoginPage() {
                     onChange={(e) => setEmail(e.target.value)} 
                     required
                   />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label">Password</label>
-                  <input 
-                    type="password" 
-                    className="form-control" 
-                    id="password" 
-                    value={password} 
-                    onChange={(e) => setPassword(e.target.value)} 
-                    required
-                  />
-                </div>
-                <div className="d-grid">
-                  <button type="submit" className="btn btn-primary">Login</button>
-                </div>
-              </form>
-              <p className="mt-3 text-center">
-                Don't have an account? <a href="/singup">Sign up here</a>
-              </p>
+  
             </div>
           </div>
         </div>
